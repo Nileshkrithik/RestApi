91d7d4563bd83392e87f73e96a195f188b63f352
Nileshkrithik <108684079+Nileshkrithik@users.noreply.github.com>
2023-08-23 15:11:35 +0530

    Add files via upload

---
+++ src/main/java/com/Nilesh/restapi/day4/repository/StudentRepo.java
@@ -0,0 +1,10 @@
+package com.Nilesh.restapi.day4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Nilesh.restapi.day4.model.StudentModel;
+@Repository
+public interface StudentRepo extends JpaRepository<StudentModel, Integer>{
+
+}
